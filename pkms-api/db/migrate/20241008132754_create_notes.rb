d02d00535ed26294d9e02c760f14270dc3073578
Zach Heckert <zach@zachheckert.com>
2024-10-08 09:56:17 -0400

    Add basic models

---
+++ pkms-api/db/migrate/20241008132754_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :content
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
