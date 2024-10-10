8e477169ab2bfe084b1f49c486d463d539c0012f
Zach Heckert <zach@zachheckert.com>
2024-10-10 11:22:34 -0400

    Flesh out notes controller. Add migration to create association between users and notes. Add a few pieces of dummy data.

--- pkms-api/app/controllers/notes_controller.rb
+++ pkms-api/app/controllers/notes_controller.rb
@@ -1,2 +1,59 @@
 class NotesController < ApplicationController
+
+  #making some notes to help me remember how things work.
+
+  # So, these methods have corresponding crud requests in the routes for my notes.
+  # I specified what routes I expect to have at this endpoint there and then add them here and flesh out the expected behavior.
+  # These methods take advantage of active record methods- We call Note and then a method.
+
+  # Index shows all my Notes
+  # Uses all to retrieve all rows from the notes table
+  def index
+    @notes = Note.all
+    render json: @notes
+  end
+
+  # The show method shows a single note
+  # Uses find(id) to grab the specific note with corresponding ID
+  def show
+    @note = Note.find(params[:id])
+    render json: @note
+  end
+
+  # creates a new note. Note that we use the note_params to make sure we're safely handling input parameters. Those are defined
+  # privately below.
+  # new isntantiates a new object without saving it to the database (follow-up with save to save it)
+  # create instnatiates and saves in a single step (i.e. Note.create(note_params))
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    # update updates one or more attributes from a record.
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: @note.errors, status: :unprocessable_entity
+    end
+  end
+
+  # destroy: AR method to delete a record.
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    head :no_content
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content, :date, :user_id)
+  end
+
 end
--- pkms-api/db/schema.rb
+++ pkms-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_08_144346) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_10_151407) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_144346) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_144346) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notes", "users"
 end
