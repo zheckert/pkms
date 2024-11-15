594d026932a946d9e20b6a0395f925c1c322331b
Zach Heckert <zach@zachheckert.com>
2024-11-15 09:22:08 -0500

    Refactor delete confirmation piece for better ux

--- pkms-frontend/src/components/DeleteButton.jsx
+++ pkms-frontend/src/components/DeleteButton.jsx
@@ -1,18 +1,17 @@
-function DeleteButton({ deleteNote, note }) {
-  //todo: continue researching this piece. Not sure window.confirm is best ux
-  const handleDelete = () => {
-    const confirmDelete = window.confirm(
-      `Are you sure you want to delete note "${note.title}"?`
-    );
-    if (confirmDelete) {
-      deleteNote(note);
-    }
-  };
+import { useState } from "react";
 
-  return (
-    <button className="delete-button" onClick={handleDelete}>
-      Delete Note
-    </button>
+function DeleteButton({ deleteNote, note }) {
+  //todo: This could use additional refinement (styling, overall ui/ux)
+  const [deletingNote, setDeletingNote] = useState(false);
+
+  return deletingNote ? (
+    <>
+      <p>Are you sure you want to delete this note?</p>
+      <button onClick={() => deleteNote(note)}>Yes</button>{" "}
+      <button onClick={() => setDeletingNote(false)}>No</button>{" "}
+    </>
+  ) : (
+    <button onClick={() => setDeletingNote(true)}>Delete Note</button>
   );
 }
 
