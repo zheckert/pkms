9e55f48e80d7d082762bd6767bfcfbbc7777319e
Zach Heckert <zach@zachheckert.com>
2024-12-04 10:41:46 -0500

    Begin removing sessions-based authentication

--- pkms-api/app/controllers/application_controller.rb
+++ pkms-api/app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::API
     # skip_before_action :verify_authenticity_token
     
-    # current_user method used for associated notes/data w/ logged-in user.
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-        current_user.present?
-    end
 end
