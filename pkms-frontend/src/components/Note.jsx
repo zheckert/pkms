7501ca2c38c88e414ba8d44b9b7756abd855a9a7
Zach Heckert <zach@zachheckert.com>
2025-01-13 11:48:54 -0500

    Reformat note content for readability

--- pkms-frontend/src/components/Note.jsx
+++ pkms-frontend/src/components/Note.jsx
@@ -22,9 +22,10 @@ function Note({ note, deleteNote }) {
   return (
     <li className="note">
       <div>
+        {/* todo: Fix note content breaking out */}
         <h2>{note.title}</h2>
-        <p>Created on {formatDate(note.created_at)}</p>
         <p>{note.content}</p>
+        <p>Created {formatDate(note.created_at)}</p>
       </div>
       <Tags tags={note.tags} />
       <DeleteButton deleteNote={deleteNote} note={note} />
