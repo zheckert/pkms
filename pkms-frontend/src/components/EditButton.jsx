6443a68d541b273b5f001506869ef56dd72bf07e
Zach Heckert <zach@zachheckert.com>
2025-02-06 11:13:30 -0500

    Continue roughing this area in

--- pkms-frontend/src/components/EditButton.jsx
+++ pkms-frontend/src/components/EditButton.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function EditButton() {
-  return <button>Edit</button>;
+  const [editingNote, setEditingNote] = useState(false);
+
+  return editingNote ? (
+    <>
+      <textarea></textarea>
+      <button onClick={() => editingNote}>Save</button>
+      <button onClick={() => setEditingNote(false)}>Cancel</button>
+    </>
+  ) : (
+    <button onClick={() => setEditingNote(true)}>Edit</button>
+  );
 }
 
 export default EditButton;
