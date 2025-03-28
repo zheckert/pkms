335a6c5404cc84776aae50e3729d4f2f5185d029
Zach Heckert <zach@zachheckert.com>
2025-03-28 11:53:20 -0400

    Remove old log

--- pkms-frontend/src/components/NoteCreationDate.jsx
+++ pkms-frontend/src/components/NoteCreationDate.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function NoteCreationDate({ note }) {
-  console.log("todo: remove IA M EHERE");
   const formatDate = (dateString) => {
     const options = {
       month: "long",
